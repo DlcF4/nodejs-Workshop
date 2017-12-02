02389d9f11915e7c3a3cf83e8d738525a053b313
DlcF4 <davide.raschetti@faq400.com>
2017-12-02 15:08:47 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var port = process.env.LITMIS_PORT_DEVELOPMENT;
+
+http.createServer(function(req, res) {  
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello World')
+}).listen(port, '0.0.0.0');
+
+console.log('Server running at http://spaces.litmis.com:%d', port)
